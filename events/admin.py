7221fa515db00dda0b5b9c9241e1dde49a18dc32
adoleyvanessa-prog <adoleyvanessa@gmail.com>
2026-03-11 17:47:56 +0000

    update admin

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Profile, Event, Booking
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'role')
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ('title', 'organiser', 'start_datetime', 'location', 'price', 'capacity')
+    search_fields = ('title', 'location', 'venue')
+    list_filter = ('location', 'start_datetime')
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('booking_reference', 'user', 'event', 'payment_status', 'status', 'created_at')
+    search_fields = ('booking_reference', 'full_name', 'email')
+    list_filter = ('payment_status', 'status', 'created_at')
